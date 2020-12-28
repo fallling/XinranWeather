e140a12309d3114d70a765b2be89357d113e9a30
fall <syaing@outlook.com>
2020-12-28 10:40:21 +0800

    Default Changelist

--- app/src/main/java/com/example/xinranweather/MainActivity.kt
+++ app/src/main/java/com/example/xinranweather/MainActivity.kt
@@ -9,7 +9,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*hello*/
         val textView:TextView = findViewById(R.id.textView)
         textView.text = "nihao"
+        textView.textSize = 18
     }
 }
