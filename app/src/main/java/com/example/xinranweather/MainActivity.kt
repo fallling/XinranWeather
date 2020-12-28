1abb546830e0e5d93b8bb23994d93c494dccde66
fall <syaing@outlook.com>
2020-12-28 22:55:39 +0800

    Default Changelist
    Signed-off-by: fall <syaing@outlook.com>

--- app/src/main/java/com/example/xinranweather/MainActivity.kt
+++ app/src/main/java/com/example/xinranweather/MainActivity.kt
@@ -1,17 +1,34 @@
 package com.example.xinranweather
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.amap.api.location.AMapLocationClient
+import com.amap.api.location.AMapLocationListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /*hello*/
-        val textView:TextView = findViewById(R.id.textView)
-        textView.text = "nihao"
-        textView.textSize = 20f
+        //异步获取定位结果
+        val mAMapLocationListener = AMapLocationListener { amapLocation ->
+            if (amapLocation != null) {
+                if (amapLocation.getErrorCode() == 0) {
+                    //可在其中解析amapLocation获取相应内容。
+                }else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError", "location Error, ErrCode:"
+                            + amapLocation.getErrorCode() + ", errInfo:"
+                            + amapLocation.getErrorInfo())
+                }
+            }
+            System.out.println(amapLocation.latitude + amapLocation.longitude)
+        }
+        //声明AMapLocationClient类对象
+        val mLocationClient = AMapLocationClient(getApplicationContext())
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mAMapLocationListener)
+        //启动定位
+        mLocationClient.startLocation()
     }
 }
---
+++ app/src/main/java/com/example/xinranweather/XinranWeatherApplication.kt
@@ -0,0 +1,24 @@
+package com.example.xinranweather
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+/**
+User: FALL
+Data: 2020/12/28
+Time: 23:05
+ */
+class XinranWeatherApplication : Application() {
+    companion object {
+        const val TOKEN = "qq8ZCrKGgM6Z6i81"
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
