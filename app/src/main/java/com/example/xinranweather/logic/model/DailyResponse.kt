9d0e2e380e33e6113c7f5dc4bb0164ea135cb0f5
fall <syaing@outlook.com>
2020-12-30 23:26:20 +0800

    搜索城市，显示天气信息
    Signed-off-by: fall <syaing@outlook.com>

--- app/src/main/java/com/example/xinranweather/logic/model/DailyResponse.kt
+++ app/src/main/java/com/example/xinranweather/logic/model/DailyResponse.kt
@@ -17,12 +17,12 @@ data class DailyResponse(val status: String, val result: Result) {
     )
 
     data class Temperature(val max: Float, val min: Float)
-    data class Skycon(val value: String, val data: Date)
+    data class Skycon(val value: String, val date: Date)
     data class LifeIndex(
-        val coldRisk: List<LifeDescription>,
-        val carWashing: List<LifeDescription>,
-        val ultraviolet: List<LifeDescription>,
-        val dressing: List<LifeDescription>
+            val coldRisk: List<LifeDescription>,
+            val carWashing: List<LifeDescription>,
+            val ultraviolet: List<LifeDescription>,
+            val dressing: List<LifeDescription>
     )
 
     data class LifeDescription(val desc: String)
--- app/src/main/java/com/example/xinranweather/logic/network/WeatherService.kt
+++ app/src/main/java/com/example/xinranweather/logic/network/WeatherService.kt
@@ -13,12 +13,14 @@ Data: 2020/12/30
 Time: 0:33
  */
 interface WeatherService {
-    @GET("v2.5/${XinranWeatherApplication.TOKEN}//{lng},{lat}/realtime.json")
+
+    //https:api.caiyunapp.com/v2.5/qq8ZCrKGgM6Z6i81/112.938814,28.228209/realtime.json
+    @GET("v2.5/${XinranWeatherApplication.TOKEN}/{lng},{lat}/realtime.json")
     fun getRealtimeWeather(
-        @Path("lng") lng: String,
-        @Path("lat") lat: String
+            @Path("lng") lng: String,
+            @Path("lat") lat: String
     ): Call<RealtimeResponse>
 
-    @GET("v2.5/${XinranWeatherApplication.TOKEN}//{lng},{lat}/daily.json")
+    @GET("v2.5/${XinranWeatherApplication.TOKEN}/{lng},{lat}/daily.json")
     fun getDailyWeather(@Path("lng") lng: String, @Path("lat") lat: String): Call<DailyResponse>
 }
